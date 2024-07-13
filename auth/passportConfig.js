14eaffe792a3101dc94366fcfb46577b553885fa
irineufreitas <freitaschoro@gmail.com>
2024-07-13 17:58:22 +0200

    callback

--- auth/passportConfig.js
+++ auth/passportConfig.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
@@ -7,7 +7,7 @@ const { findUserByEmail, createUser } = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
