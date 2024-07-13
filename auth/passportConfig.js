bf03dd3d0aa86afcee2704c7a0ed9d931716a2ff
irineufreitas <freitaschoro@gmail.com>
2024-07-13 17:49:16 +0200

    callback

--- auth/passportConfig.js
+++ auth/passportConfig.js
@@ -7,7 +7,7 @@ const { findUserByEmail, createUser } = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: "http://localhost:3300/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
